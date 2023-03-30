bff0001d3fb9e6285d7e58c136fb719c986b00c3
Barkha Rawat <barkha9814@gmail.com>
2023-03-31 00:38:42 +0530

    Frontend layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
+import './App.scss';
+import 'boxicons/css/boxicons.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import Home from './pages/homePage';
+import User from './pages/userPage';
+import Room from './pages/roompage';
 
 function App() {
-  return (
-    <div className="App">
-     
-        <p>
-            Schedule Management System
-        </p>
-       
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/user' element={<User />} />
+                    <Route path='/room' element={<Room />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ client/src/pages/userPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userPage() {
+  return (
+    <div>User Page</div>
+  )
+}
+
+export default userPage
